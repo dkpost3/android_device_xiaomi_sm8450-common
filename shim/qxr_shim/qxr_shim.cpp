43b1df7b717412e9c509d839bff7d51d1beb8f02
Dmitry <42121541+dkpost3@users.noreply.github.com>
2025-03-08 04:23:29 +0300

    initialize crdroid 11.2

---
+++ shim/qxr_shim/qxr_shim.cpp
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+namespace android {
+    extern "C" void  _ZN4aidl7android8hardware6common12NativeHandle14readFromParcelEPK7AParcel() {}
+    extern "C" void  _ZNK4aidl7android8hardware6common12NativeHandle13writeToParcelEP7AParcel() {}
+}
